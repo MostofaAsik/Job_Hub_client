333ea91aea290c4fd17ad18c0ce13694cd7d0300
MostofaAsik <mmostofaasik@gmail.com>
2025-01-30 16:21:29 +0600

    config file hide

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBnVAqRtjjsylSg-0u9BtDz9_DBlOYXEwo",
-  authDomain: "job-hub-9e827.firebaseapp.com",
-  projectId: "job-hub-9e827",
-  storageBucket: "job-hub-9e827.firebasestorage.app",
-  messagingSenderId: "1006992844083",
-  appId: "1:1006992844083:web:26d1e6c1dff03d29274a46"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 const app = initializeApp(firebaseConfig);
