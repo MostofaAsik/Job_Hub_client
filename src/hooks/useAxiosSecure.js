08fe6ca6a8399940d85a386f60308cc98f79438d
MostofaAsik <mmostofaasik@gmail.com>
2025-02-25 17:33:24 +0600

    done

--- src/hooks/useAxiosSecure.js
+++ src/hooks/useAxiosSecure.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
@@ -6,6 +9,25 @@ const axiosInstance = axios.create({
 });
 
 const useAxiosSecure = () => {
+    const { signOutUser } = useAuth()
+    const navigate = useNavigate()
+
+
+    useEffect(() => {
+        axiosInstance.interceptors.response.use(
+            (response) => response,
+            (error) => {
+                if (error.response.status === 401 || error.response.status === 403) {
+                    console.log("Unauthorized request. Redirecting to login page...");
+                    signOutUser();
+                    navigate('/login')
+
+                }
+                return Promise.reject(error);
+            }
+        );
+    }, [])
+
     return axiosInstance;
 };
 
